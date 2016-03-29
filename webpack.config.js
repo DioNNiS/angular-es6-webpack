3e6f34cf1f630e5559c0ba408b604a653522ddb8
Denis Liger <denis.liger@genesys.com>
2016-03-29 10:44:06 -0700

    Added "compression-webpack-plugin" plugin

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ var autoprefixer = require('autoprefixer');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var CopyWebpackPlugin = require('copy-webpack-plugin');
+var CompressionPlugin = require("compression-webpack-plugin");
 
 /**
  * Env
@@ -216,7 +217,17 @@ module.exports = function makeWebpackConfig() {
             // Reference: https://github.com/kevlened/copy-webpack-plugin
             new CopyWebpackPlugin([{
                 from: __dirname + '/app/public'
-            }])
+            }]),
+
+            // Compress assets
+            // Reference: https://github.com/webpack/compression-webpack-plugin
+            new CompressionPlugin({
+                asset: "[path].gz[query]",
+                algorithm: "gzip",
+                test: /\.js$|\.css$|\.html$/,
+                threshold: 10240,
+                minRatio: 0.8
+            })
         )
     }
 
